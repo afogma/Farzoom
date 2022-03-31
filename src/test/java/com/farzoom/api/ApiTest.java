4a977433f8aa7c6ebe20c6e08d67d7266fb028aa
fogma <fogmatic@gmail.com>
2022-03-31 15:01:31 +0300

    api test added

---
+++ src/test/java/com/farzoom/api/ApiTest.java
@@ -0,0 +1,42 @@
+package com.farzoom.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+//import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles(profiles = "test")
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ApiTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void createTaskAndGet() throws Exception {
+        MockHttpServletRequestBuilder createTask = MockMvcRequestBuilders.post("/api/task")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"name\":\"testTask\", \"description\":\"description\"}");
+
+        mockMvc.perform(createTask)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)));
+
+        MockHttpServletRequestBuilder getTasks = MockMvcRequestBuilders.get("/api/tasks");
+        mockMvc.perform(getTasks)
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$", hasSize(1)));
+    }
+}
