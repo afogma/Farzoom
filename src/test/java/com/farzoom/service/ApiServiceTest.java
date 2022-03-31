0bc30ed953d4b3feb737e704b3266452772ba825
fogma <fogmatic@gmail.com>
2022-03-31 11:15:53 +0300

    changes in repo/service/controller

---
+++ src/test/java/com/farzoom/service/ApiServiceTest.java
@@ -0,0 +1,32 @@
+package com.farzoom.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ApiServiceTest {
+
+    @Test
+    void findAllTasks() {
+    }
+
+    @Test
+    void findTaskById() {
+    }
+
+    @Test
+    void deleteTask() {
+    }
+
+    @Test
+    void updateTasksName() {
+    }
+
+    @Test
+    void updateTasksDescription() {
+    }
+
+    @Test
+    void createNewTask() {
+    }
+}
